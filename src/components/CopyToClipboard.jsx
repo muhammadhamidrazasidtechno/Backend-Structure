d2308ff94c5f5abe86b58f4a35e4b5e3887b2ee8
Irtiza Ahmed <hamidsidtechno@gmail.com>
2024-07-05 18:47:43 +0500

    push

---
+++ src/components/CopyToClipboard.jsx
@@ -0,0 +1,30 @@
+// ClipboardCopy.js
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+
+const ClipboardCopy = ({ text }) => {
+  const linkRef = useRef(null);
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const copyToClipboard = () => {
+    const link = linkRef.current.innerText;
+    navigator.clipboard.writeText(link);
+    setCopySuccess(true);
+    setTimeout(() => {
+      setCopySuccess(false);
+    }, 3000); // Reset copy success message after 3 seconds
+  };
+
+  return (
+    <div className='flex box-box justify-between relative'>
+      <a ref={linkRef} href={text} className="text-blue-500 hover:underline hidden">
+        {text}
+      </a>{' '}
+      <FontAwesomeIcon icon={faCopy} onClick={copyToClipboard} className="copy-icon" />
+      {copySuccess && <p className="copy-success-message ">Link copied!</p>}
+    </div>
+  );
+};
+
+export default ClipboardCopy;
--- src/components/StudentSignup.jsx
+++ src/components/StudentSignup.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import '../app/StudentSignup.css'; // Import the CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import ClipboardCopy from './CopyToClipboard';
 
 const StudentSignup = () => {
   return (
     <div className="signup-container">
       <div className="signup-section">
         <label className="signup-label">Student Signup Link</label>
-        <p className="signup-text">Link: <a href="https://links-ehps.onrender.com/student/signup" className="text-blue-500 hover:underline">https://links-ehps.onrender.com/student/signup</a></p>
+        <p className="signup-text">
+          Link:{' '}
+          <div className='box-box'>
+          https://links-ehps.onrender.com/student/signup
+          <ClipboardCopy text="https://links-ehps.onrender.com/student/signup" />
+          </div>
+          
+        </p>
       </div>
       <div className="signup-section">
         <label className="signup-label">Link Method</label>
@@ -14,17 +24,22 @@ const StudentSignup = () => {
       </div>
       <div className="signup-section">
         <label className="signup-label">Body</label>
-        <div className="signup-body">
-          <p className="signup-body-item">"username": "rAZAmuhiuffin",</p>
-          <p className="signup-body-item">"email": "HAMID@gmail.com",</p>
-          <p className="signup-body-item">"password": "123456",</p>
-          <p className="signup-body-item">"phone": "0330271580",</p>
-          <p className="signup-body-item">"cnic": "42301999434302",</p>
-          <p className="signup-body-item">"lastActivity": "2024-06-26"</p>
+        <div className="signup-body flex">
+          <div>
+            <p className="signup-body-item">"username": "rAZAmuhiuffin",</p>
+            <p className="signup-body-item">"email": "HAMID@gmail.com",</p>
+            <p className="signup-body-item">"password": "123456",</p>
+            <p className="signup-body-item">"phone": "0330271580",</p>
+            <p className="signup-body-item">"cnic": "42301999434302",</p>
+            <p className="signup-body-item">"lastActivity": "2024-06-26"</p>
+          </div>
+          <div>
+            <ClipboardCopy text='"username": "rAZAmuhiuffin","email": "HAMID@gmail.com","password": "123456","phone": "0330271580","cnic": "42301999434302","lastActivity": "2024-06-26"' />
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default StudentSignup;
